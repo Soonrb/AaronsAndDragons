058d91c654d2b1292049ad97bf990fdca3adca4d
Soonrb <Soonrb@gmail.com>
2014-06-06 02:32:15 -0400

    Made 10x10 pixel boxes in 500x500 window

--- player.rb
+++ player.rb
@@ -1,62 +1,5 @@
 # encoding: utf-8
 
-class Map
-
-    width = 64
-    height = 32
-    $path = " "
-    $abyss = "█"
-    $playerChar = 'o'
-    position = []
-    message = ""
-
-    def new_game(level, w, h)
-        startX = rand(0...h)
-        startY = rand(0...w)
-
-        start_location,$position = level[startX][startY]
-        if start_location == $path
-            level[startX][startY] = $playerChar
-    		
-    		return level, [startX, startY]
-    	
-        else
-            new_game(level, w, h)
-        end
-    end
-
-    def makeLevel(w,h)
-        level = Array.new
-        i = 0
-        while i < h
-            level[i] = Array.new
-
-            j = 0
-
-            while j < w
-
-                random_number = [0, 1].sample
-
-                if random_number == 0
-                    level[i][j] = $path
-                else
-                    level[i][j] = $abyss
-                end 
-
-                j += 1
-            end
-            i += 1
-        end
-        return level
-    end
-
-    def showLevel(level)
-        level.each { |x|
-            puts x.join("")
-        }
-    end
-end
-
 class Player
 
     def up(pos,level)
@@ -106,19 +49,3 @@ class Player
     showLevel(level)
     end
 end
-
-level = makeLevel(width,height)
-level, position = new_game(level, width, height)
-puts position
-showLevel(level)
-
-
-
-
-
-
-
-
-
-
-
--- test.rb
+++ test.rb
@@ -1,19 +1,87 @@
+# encoding: utf-8
+
+require 'rubygems'
 require 'gosu'
+load 'level.rb'
 
 class GameWindow < Gosu::Window
-  def initialize
-    super(640, 480, false)
-    self.caption = "Gosu Tutorial Game"
+    def initialize
+        super 500, 500, false
+        self.caption = "Aarons & Dragons"
+                
+        @level = Level.new
+        @pathColor = Gosu::Color.argb(0xff0b2217) #witchcraft colours = 0xff3e8129 - 0xff = #3e8129 = RRGGBB
+    end
 
-    @background_image = Gosu::Image.new(self, "media/Space.png", true)
-  end
 
-  def update
-  end
+    def update
+        
+    end
 
-  def draw
-    @background_image.draw(0, 0, 0)
-  end
+    def draw
+
+        x1 = 0 # top left
+        y1 = 0
+        
+        x2 = 10 # bottom left
+        y2 = 0
+        
+        x3 = 0 # top right
+        y3 = 10
+        
+        x4 = 10 # bottom right
+        y4 = 10
+
+        plane_x = [x1, x2, x3, x4]
+
+        plane_y = [y1, y2, y3, y4]
+
+        j = 0
+
+        while j < 500
+
+        while plane_y[0] < 501
+
+          draw_quad(
+            plane_x[0]+j,plane_y[0]+j,Gosu::Color.argb(0xff80d5c7),
+            plane_x[1]+j,plane_y[1]+j,@pathColor,
+            plane_x[2]+j,plane_y[2]+j,@pathColor,
+            plane_x[3]+j,plane_y[3]+j,Gosu::Color.argb(0xff80d5c7),
+            0)
+
+            plane_y = plane_y.collect! { |c|
+            c + 10
+            }
+        end
+
+        plane_y = [y1, y2, y3, y4]   
+
+          while plane_x[0] < 501
+
+          draw_quad(
+            plane_x[0]+j,plane_y[0]+j,Gosu::Color.argb(0xff80d5c7),
+            plane_x[1]+j,plane_y[1]+j,@pathColor,
+            plane_x[2]+j,plane_y[2]+j,@pathColor,
+            plane_x[3]+j,plane_y[3]+j,Gosu::Color.argb(0xff80d5c7),
+            0)
+
+            plane_x = plane_x.collect! { |c|
+            c + 10
+            }
+        end
+
+        plane_x = [x1, x2, x3, x4]
+
+        j += 10
+
+        end
+    end
+
+    def button_down(id)
+        if id == Gosu::KbEscape
+            close
+        end
+    end
 end
 
 window = GameWindow.new
